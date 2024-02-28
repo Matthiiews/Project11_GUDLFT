9661bb08bb697903f302595d27b33ba77702c387
Matthiiews <matthieu.kouassi@gmail.com>
2024-02-28 15:27:47 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,57 +1,8 @@
-import json
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, flash, url_for
 
-
-def loadClubs():
-    with open('clubs.json') as c:
-        listOfClubs = json.load(c)['clubs']
-        return listOfClubs
-
-
-def loadCompetitions():
-    with open('competitions.json') as comps:
-        listOfCompetitions = json.load(comps)['competitions']
-        return listOfCompetitions
-
-
-def sortCompetitionsDate(comps):
-    past = []
-    present = []
-
-    for comp in comps:
-        compDate = datetime.strptime(comp['date'], '%Y-%m-%d %H:%M:%S')
-        if compDate < datetime.now():
-            past.append(comp)
-        elif compDate >= datetime.now():
-            present.append(comp)
-
-    return past, present
-
-
-def initializeBookedPlaces(comps, clubsList):
-    places = []
-    for comp in comps:
-        for club in clubsList:
-            places.append(
-                {'competition': comp['name'], 'booked': [0, club['name']]})
-
-    return places
-
-
-def updateBookedPlaces(competition, club, placesBooked, placesRequired):
-    for item in placesBooked:
-        if item['competition'] == competition['name'] and item[
-                'booked'][1] == club['name']:
-            if item['booked'][0] + placesRequired <= 12:
-                item['booked'][0] += placesRequired
-                break
-            else:
-                raise ValueError(
-                    "You can't book more than 12 places in a competition.")
-
-    return placesBooked
-
+from utils import (loadClubs, loadCompetitions, sortCompetitionsDate,
+                   initializeBookedPlaces, updateBookedPlaces)
 
 app = Flask(__name__)
 app.secret_key = 'something_special'
